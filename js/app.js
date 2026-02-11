e4d6ce5b80edc018244eddc17504bbd144a659fd
sidiqsck-oss <sidiq.sck@gmail.com>
2026-02-11 14:15:16 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -298,7 +298,7 @@
 
     // --- Service Worker Registration ---
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('/feedlotmanagement/service-worker.js')
             .then(reg => console.log('SW registered:', reg.scope))
             .catch(err => console.warn('SW registration failed:', err));
     }
